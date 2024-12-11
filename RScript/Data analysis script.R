907422665e4f4e5bb8a5d7dc5b9d47543dbac336
Will Andrew <WA278394@falmouth.ac.uk>
2024-12-11 20:25:14 +0000

    i hate that fork keeps doing this

--- RScript/Data analysis script.R
+++ RScript/Data analysis script.R
@@ -1,10 +1,14 @@
-#assign variables to the csv data being read
-env1Data <- read.csv('/CSV/Envrion1Data')
-env2Data <- read.csv('/CSV/Environ2Data')
-env3Data <- read.csv('/CSV/Environ3Data')
+readfile1 <- read.csv("Environ1Data.csv")
 
-#TODO: fix this to use our variables
-# Compute the analysis of variance
-res.aov <- aov(weight ~ group, data = my_data)
-# Summary of the analysis
-summary(res.aov
+
+#Data_Frame <- data.frame(
+  #Algorithm = c("Frustum View", "Back-Face", "Occlusion", "Combined", "None"),
+  #PolyCount = c("1000", "800", "1200", "300", "1600"),
+  #FPS = c("132", "144", "120", "144", "106"),
+  #GPU_Usage = c("30%", "25%", "35%", "20%", "40%")
+#)
+
+#summary(Data_Frame)
+
+
+
