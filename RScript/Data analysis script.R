6ae5aaf358b9734eb455917bdb34e0502c0b6d57
WILL ANDREW <WA278394@falmouth.ac.uk>
2024-12-11 18:49:17 +0000

    added a TODO in the R code

--- RScript/Data analysis script.R
+++ RScript/Data analysis script.R
@@ -3,3 +3,8 @@ env1Data <- read.csv('/CSV/Envrion1Data')
 env2Data <- read.csv('/CSV/Environ2Data')
 env3Data <- read.csv('/CSV/Environ3Data')
 
+#TODO: fix this to use our variables
+# Compute the analysis of variance
+res.aov <- aov(weight ~ group, data = my_data)
+# Summary of the analysis
+summary(res.aov
