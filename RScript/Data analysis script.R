454420f7ce7bd4c71baab2ea08cff734787fbf7e
WILL ANDREW <WA278394@falmouth.ac.uk>
2024-12-11 18:38:59 +0000

    oops

--- RScript/Data analysis script.R
+++ RScript/Data analysis script.R
@@ -1,4 +1,4 @@
-#assign variables to the csv data
+#assign variables to the csv data being read
 env1Data <- read.csv('/CSV/Envrion1Data')
 env2Data <- read.csv('/CSV/Environ2Data')
 env3Data <- read.csv('/CSV/Environ3Data')
