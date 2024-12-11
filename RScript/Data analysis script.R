06989245a69e2c12deaff3307552b5bd615353f3
Will Andrew <WA278394@falmouth.ac.uk>
2024-12-11 21:33:05 +0000

    done the R code

--- RScript/Data analysis script.R
+++ RScript/Data analysis script.R
@@ -1,14 +1,29 @@
-readfile1 <- read.csv("Environ1Data.csv")
+library(ggplot2)
+
+dataCSV <- read.csv("Environ1Data.csv")
+xValue <- dataCSV[, 2]
+yValue <- dataCSV[, 5]
+df <- data.frame(xValue,yValue)
+
+ggplot(df, aes(x = xValue, y = yValue)) + geom_line() +
+  ggtitle("Graph to show Polygon count against RAM usage") +
+  xlab("Polygon Count") + 
+  ylab("RAM usage")
+
+res.aov <- aov(Algorithm ~ PolCount + RAM, data = dataCSV)
+summary(res.aov)
 
 
-#Data_Frame <- data.frame(
-  #Algorithm = c("Frustum View", "Back-Face", "Occlusion", "Combined", "None"),
-  #PolyCount = c("1000", "800", "1200", "300", "1600"),
-  #FPS = c("132", "144", "120", "144", "106"),
-  #GPU_Usage = c("30%", "25%", "35%", "20%", "40%")
+
+#print(cat("polygons are rendered per unit of RAM", dataCSV[1, 2] / dataCSV[1, 5]))
+#print(cat("polygons are rendered per unit of GPU usage", dataCSV[1, 2] / dataCSV[1, 4]))
+
+#print(cat("frames per second are rendered per unit of RAM", dataCSV[1, 3] / dataCSV[1, 5]))
+#print(cat("frames per second are rendered per unit of GPU usage", dataCSV[1, 3] / dataCSV[1, 4]))
+
+#plot(x = dataCSV$RAM, y = dataCSV$PolCount,
+#xlab = "Ram Usage",
+#ylab = "PolCount",
+#main = "Polygon Count against Ram Usage"
 #)
-
-#summary(Data_Frame)
-
-
 
