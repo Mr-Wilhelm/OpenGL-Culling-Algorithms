ca04ef3c805666454b7b541d8860e38da64bd967
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-01-23 12:46:27 +0000

    adding all the libraries to the c++ project

---
+++ OpenGLTest5/Main.cpp
@@ -0,0 +1,16 @@
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+#include <fstream>
+#include <sstream> 
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
+
+
+
+
+
