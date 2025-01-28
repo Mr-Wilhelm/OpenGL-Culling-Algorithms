529139d05e28ffd11389b80780d4a08c2fbd28e5
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-01-28 12:43:37 +0000

    fixed texture

--- COMP303Artefact/Main.cpp
+++ COMP303Artefact/Main.cpp
@@ -174,7 +174,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char* data = stbi_load("D:\COMP302-WA278394-2204080\COMP303Artefact\white", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("white.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -196,7 +196,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("D:\COMP302-WA278394-2204080\COMP303Artefact\white", &width, &height, &nrChannels, 0);
+    data = stbi_load("white.png", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
