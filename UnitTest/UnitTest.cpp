1b5256452cc63fb72988a98d47a5b3dbaf0c1148
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-03-05 10:57:15 +0000

    linked library and dependency files, no errors so far

---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
+
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+
+
+//#include "Shader.h"
+//#include "Camera.h"
+//#include "Model.h"
+//#include "CamView.h"
+//#include "CullingAlgorithms.h"
+
+#include<list>
+#include<memory>
+
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
