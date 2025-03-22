7ad7e2a930b7566b68d4d6f74b1436f456bf10c9
Will Andrew <WA278394@falmouth.ac.uk>
2025-03-22 15:04:41 +0000

    startind dynamic z data

--- COMP303Artefact/Main.cpp
+++ COMP303Artefact/Main.cpp
@@ -73,7 +73,7 @@ float offsetModelPos = 4.0f;
 
 //data gathering variables
 int iteration = 1;
-std::string fileName = "Env_1_ZCulling_Averages.csv";
+std::string fileName = "Env_2_ZCulling_Averages.csv";
 std::list<std::string> dataList;
 unsigned long int globalModelsZCulled = 0;
 unsigned long long int globalPolysZCulled = 0;
@@ -189,8 +189,8 @@ int main()
     //------CHOICES: DENSE, SPARSE, DYNAMIC------
 
     //chosenEnvironment = DENSE;
-    chosenEnvironment = SPARSE;
-    //chosenEnvironment = DYNAMIC;
+    //chosenEnvironment = SPARSE;
+    chosenEnvironment = DYNAMIC;
     //chosenEnvironment = DEFAULT;
 
     //-------------------------------------------
