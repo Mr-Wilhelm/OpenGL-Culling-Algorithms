367342fde0c02308ad56e36eeb57e0ebd98b85f3
WILL ANDREW <WA278394@falmouth.ac.uk>
2025-03-25 14:53:04 +0000

    started R coding

---
+++ RScript/RDataAnalysis.R
@@ -0,0 +1,15 @@
+env0NoCulling <- read.csv(Env_0_NoCulling_Averages)
+env1NoCulling <- read.csv()
+env2NoCulling <- read.csv()
+
+env0Frustum <- read.csv()
+env1Frustum <- read.csv()
+env2Frustum <- read.csv()
+
+env0Back <- read.csv()
+env1Back <- read.csv()
+env2Back <- read.csv()
+
+env0Z <- read.csv()
+env1Z <- read.csv()
+env2Z <- read.csv()
