9ea06b3d51d87a4b53d8424334e9a728fffb225b
Will Andrew <WA278394@falmouth.ac.uk>
2025-03-25 13:08:16 +0000

    env 0 all culling done

--- COMP303Artefact/Main.cpp
+++ COMP303Artefact/Main.cpp
@@ -68,8 +68,8 @@ float offsetTime = 1.0f;
 float offsetModelPos = 4.0f;
 
 //data gathering variables
-int iteration = 38;
-std::string fileName = "Env_2_BackfaceAndZ_Averages.csv";
+int iteration = 1;
+std::string fileName = "Env_1_AllCulling_Averages.csv";
 std::list<std::string> dataList;
 unsigned long int globalModelsZCulled = 0;
 unsigned long long int globalPolysZCulled = 0;
@@ -173,8 +173,8 @@ int main()
     //------CHOICES: DENSE, SPARSE, DYNAMIC------
 
     //chosenEnvironment = DENSE;
-    //chosenEnvironment = SPARSE;
-    chosenEnvironment = DYNAMIC;
+    chosenEnvironment = SPARSE;
+    //chosenEnvironment = DYNAMIC;
     //chosenEnvironment = DEFAULT;
 
     //-------------------------------------------
